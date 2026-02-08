bbe3341b704948044f3c3d1a4bc0bdc4898c1ac6
Dakshs-07 <dakshpinjore@gmail.com>
2026-02-08 22:37:18 +0530

    latest design! updated styling for navbars and member profiles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div>
         <Image src="/main/banner.webp" alt='stray kids banner' height={2000} width={2000} />
-        <h1 className='text-center tracking-widest text-2xl font-light py-5'>STRAY KIDS FANDOM</h1>
+        <h1 className='text-center tracking-widest text-6xl font-light py-10'>STRAY KIDS FANDOM</h1>
     </div>
   )
 }
--- src/components/Main/Members.jsx
+++ src/components/Main/Members.jsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 function Members() {
   return (
     <div className="py-10">
-      <h1 className="text-center tracking-wider font-extralight text-2xl mb-8">
+      <h1 className="text-center tracking-wider font-extralight text-2xl mb-6">
         Members
       </h1>
       <div className="flex gap-x-10 justify-center items-center">
         {memberData.map((item) => (
-          <div id={item.id} className="text-center">
+          <div key={item.id} className="text-center">
             <div className="w-25 h-25 rounded-full overflow-hidden border border-gray-200 shadow-sm transition-transform duration-300 group-hover:scale-105">
               <Image
                 src={item.img}
