0bce5b0c2740b92c5ba6d5950d100b2feed7be85
Dakshs-07 <dakshpinjore@gmail.com>
2026-02-11 22:45:48 +0530

    updated styling for yt section and api limit to max 4 videos

--- src/components/Main/LatestVideos.jsx
+++ src/components/Main/LatestVideos.jsx
@@ -10,28 +10,41 @@ export default function LatestVideos() {
       .then((data) => {
         setVideos(data.items);
       });
-
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {videos.map((video) => (
-        <a
-          key={video.id.videoId}
-          href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
-          target="_blank"
-          className="group"
-        >
-          <img
-            src={video.snippet.thumbnails.high.url}
-            alt={video.snippet.title}
-            className="rounded-xl"
-          />
-          <h3 className="mt-2 text-sm font-semibold group-hover:text-gray-500">
-            {video.snippet.title}
-          </h3>
-        </a>
-      ))}
+    <div className="my-10 py-2">
+      <h1 className="text-center text-4xl my-2 tracking-wide">
+        Latest Youtube Content
+      </h1>
+      <p className="text-center text-muted-foreground text-lg mb-10">
+        From the official{" "}
+        <a href="https://www.youtube.com/channel/UC9rMiEjNaCSsebs31MRDCRA">
+          <span className="text-(--color-chart-1) underline decoration-wavy">
+            @StrayKids
+          </span>
+        </a>{" "}
+        YouTube Channel
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {videos.map((video) => (
+          <a
+            key={video.id.videoId}
+            href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+            target="_blank"
+            className="group"
+          >
+            <img
+              src={video.snippet.thumbnails.high.url}
+              alt={video.snippet.title}
+              className="rounded-xl"
+            />
+            <h3 className="mt-2 text-sm font-semibold group-hover:text-gray-500">
+              {video.snippet.title}
+            </h3>
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
