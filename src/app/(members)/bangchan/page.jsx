a4a8ee9ecbd9c060505c563ffc93d38abb699725
Dakshs-07 <dakshpinjore@gmail.com>
2026-02-12 22:23:27 +0530

    created routes ofr all member pages and added leeknow's data

--- src/app/(members)/bangchan/page.jsx
+++ src/app/(members)/bangchan/page.jsx
@@ -1,8 +1,8 @@
 import MemberTable from "@/components/Member/MemberTable";
 import React from "react";
-import { LeeKnowData } from "../../../../data/Table";
+import { BangchanData } from "../../../../data/Table";
 import FunFacts from "@/components/Member/FunFacts";
-import { LeeknowFacts } from "../../../../data/FunFacts";
+import { BangchanFacts } from "../../../../data/FunFacts";
 function page() {
   return (
     <div className="max-w-4xl mx-auto px-6 py-12">
@@ -14,9 +14,9 @@ function page() {
             height={200}
             className="object-cover"
           /> */}
-        <MemberTable data={LeeKnowData} />
+        <MemberTable data={BangchanData} />
       </div>
-      <FunFacts factData={LeeknowFacts}/>
+      <FunFacts factData={BangchanFacts} />
     </div>
   );
 }
