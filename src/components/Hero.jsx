1ce7e0be0a8da2079ab0b35520fcc6b0fac7250a
Dakshs-07 <dakshpinjore@gmail.com>
2026-02-12 12:26:31 +0530

    added hero title and korean names for members

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,21 +4,21 @@ import React from "react";
 import { ImageCarousel } from "./Main/ImageCarousel";
 import memberData from "../../data/members";
 import { usePathname } from "next/navigation";
+import { HeroTitles } from "../../data/members";
 
 function Hero() {
     const pathname = usePathname();
     const page = usePathname === "/" ? "home" : pathname.replace("/", "");
-
+    const heroName = HeroTitles.find((item)=>item.page===page);
   return (
-    
     <div>
-      <ImageCarousel page={page}/>
+      <ImageCarousel page={page} />
       <div className="relative flex items-center justify-center py-10">
         <h1 className="relative z-10 text-center tracking-widest text-6xl font-light sm:text-2xl md:text-4xl lg:text-6xl">
-          STRAY KIDS FANDOM
+          {heroName.name}
         </h1>
         <h1 className="absolute text-9xl text-muted-foreground opacity-10 z-0 sm:text-5xl md:text-7xl">
-          스트레이 키즈
+          {heroName.korean}
         </h1>
       </div>
     </div>
