cd40c21124a8c1927dd1a4f5c9f4fe74e82f6aeb
Dakshs-07 <dakshpinjore@gmail.com>
2026-02-12 22:21:57 +0530

    added member data and member facts

---
+++ data/FunFacts.js
@@ -0,0 +1,74 @@
+export const BangchanFacts = [
+    "He is the eldest sibling of three.",
+    "He is credited with writing some of the most innocent SKZ songs",
+    "He created Stray Kids group, logo and chose the members himeself.",
+    "Stays prefer Chan's room over therapy",
+    "Bangchan is the eldest of all SKZ members too.",
+    "He has a dog, Berry",
+]
+
+
+
+export const LeeknowFacts = [
+    "His name means 'vast sky'",
+    "He is known as butt-hunter of the group.",
+    "Lee Know has three cats - Soonie, Doongie and Dori. His cats are very important to him and not knowing how many cats he has upsets him.",
+    "Lee Know loves pudding",
+    "Lee Know is a brand ambassador for Gucci",
+    "A very well known member of paboracha",
+    "He is very close friends with Han Jisung and both consider each other their soulmate.",
+    "He knows boxing and shooting.",
+    "Lee Know used to be a backup dancer for BTS before joining JYP.",
+    "He auditioned for JYP in 2015, but did not hear back until 2017.",
+    "(not a fun fact at all) He was one of the two members eliminated in the survival show. After the show, he walked through the rain as he 'didn't think he deserved a taxi'",
+    "He is an only child."
+]
+
+export const ChangbinFacts = [
+    "He has an older sister",
+    "He is considered the babygirl of the group",
+    "He is known for his muscles",
+    "He loves chicken.",
+    "Changbin, Han and BangChan form the renowned 3Racha",
+]
+
+export const HyunjinFacts = [
+    "Hyunjin has a pet dog Kammi",
+    "He is an only child.",
+    "Hyunjin loves painting and has made several stellar pieces.",
+    "Hyunjin's favorite place to visit is museums.",
+    "Hyunjin is known for being the idol who asbolutely rocked the buzzcut.",
+    "",
+]
+
+export const FelixFacts = [
+    "Felix is kwnown for his deep voice.",
+    "Felix is a midde child - he has 2 sisters, Olivia and Racheal.",
+    "Felix loves brownies. He is good at baking, too.",
+    "He is a brand ambassador for multiple luxury and fashion brands.",
+    "He is known for rocking unisex looks and style.",
+]
+
+export const HanFacts = [
+    "He is known as the babygirl of the group.",
+    "He can rap, sing, compose, dance, produce - which is why people call him the 'ace' of Kpop.",
+    "Han loves dinosaurs.",
+    "Han and Lee Know live together. He eats everything Lee Know cooks. Togther they like going to cafes and calm places.",
+    "Pre-debut Han would cross with people, Hyunjin in particular. Now they get along really well.",
+    "Han is known for his tiny waist (70 cm).",
+    "Han's small mouth makes him look like a cute little squirrel when he eats."
+]
+
+export const SeungminFacts = [
+    "Seungmin has an elder sister.",
+    "He is knownw for hanging out away from the group, fascinated by other things.",
+    "He is know for 'Seungming in the building', and has been seen in background of various JYP videos filmed in the building.",
+    "His 'Hmmm...  ascending I take off touch the sun' can make people levitate.",
+    "He loves coffee machines.",
+    "He enjoys photography and is really good at it.",
+    "He is known for his boba eyes."
+]
+
+export const InFacts = [
+    "He is the maknae of the group.",
+]
--- data/Table.js
+++ data/Table.js
@@ -1,8 +1,50 @@
+const LeeKnowMainPhoto = "/memberPages/LeeKnow/leeknow-main.webp"
+
+// ======================member data for member info pages ======================
+// to do:
+// add english names
+
+export const BangchanData = [
+  { label: "Stage Name", value: "Bangchan" },
+  { label: "Birth Name", value: "Bnag Chan" },
+  { label: "Birthday", value: "October 25, 1998" },
+  { label: "Position", value: "Leader, Rapper" },
+  { label: "MBTI", value: "need to update" },
+  { label: "Emoji", value: "🐰" },
+  { label: "Nationality", value: "Australian" },
+  { label: "Rpresentative Animal", value: "WolfChan" },
+];
+
 export const LeeKnowData = [
   { label: "Stage Name", value: "Lee Know" },
   { label: "Birth Name", value: "Lee Minho" },
   { label: "Birthday", value: "October 25, 1998" },
   { label: "Position", value: "Vocalist, Dancer" },
+  { label: "MBTI", value: "need to update" },
+  { label: "Emoji", value: "🐰" },
   { label: "Nationality", value: "Korean" },
-  { label: "Skzoo", value: "Leebit" },
+  {label: "English Name", value:"Rhino"},
+  { label: "Rpresentative Animal", value: "Leebit" },
 ];
+export const ChangbinData = [
+  { label: "Stage Name", value: "Changbin" },
+  { label: "Birth Name", value: "Seo Changbin" },
+  { label: "Birthday", value: "October 25, 1998" },
+  { label: "Position", value: "Rapper" },
+  { label: "MBTI", value: "need to update" },
+  { label: "Emoji", value: "🐰" },
+  { label: "Nationality", value: "Korean" },
+  { label: "Rpresentative Animal", value: "Dwakkei" },
+];
+
+export const HyunjinData = [
+  { label: "Stage Name", value: "Hyunijn" },
+  { label: "Birth Name", value: "Lee Minho" },
+  { label: "Birthday", value: "October 25, 1998" },
+  { label: "Position", value: "Vocalist, Dancer" },
+  { label: "MBTI", value: "need to update" },
+  { label: "Emoji", value: "🐰" },
+  { label: "Nationality", value: "Korean" },
+  { label: "Rpresentative Animal", value: "Leebit" },
+];
+
