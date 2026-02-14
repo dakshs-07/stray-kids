53d6756756ac90e367f04038bf7fc31f953ed1df
Dakshs-07 <dakshpinjore@gmail.com>
2026-02-14 13:08:04 +0530

    updated Leftbar to have content relavant to the current page and made them stickky

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -1,17 +1,34 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import NavTable from "./Member/NavTable";
+import memberData from "../../data/members";
 
 function LeftBar() {
+  const pathname = usePathname();
+  const currentPage = pathname.replace("/", "");
+
+  const isMemberPage = memberData.some((member) => member.link === pathname);
+
+  if (isMemberPage) {
+    return (
+      <div className="max-w-70 p-1 ml-10 sticky top-0 h-screen">
+        <NavTable member={currentPage} />
+      </div>
+    );
+  }
+
   return (
-    <div className="max-w-70 p-1 ml-10">
+    <div className="max-w-70 p-1 ml-10 sticky top-0 h-screen">
       <div className="bg-black h-6 w-full text-white px-2 tracking-wide">
         Specials
       </div>
       <div className="px-1 gap-y-1 flex flex-col sm:text-sm">
-        <Link href="/">Lee Know</Link>
-        <Link href="/">Lee Know</Link>
-        <Link href="/">Lee Know</Link>
-        <Link href="/">Lee Know</Link>
+        <Link href="/leeknow">Lee Know</Link>
+        <Link href="/bangchan">Bangchan</Link>
+        <Link href="/changbin">Changbin</Link>
+        <Link href="/hyunjin">Hyunjin</Link>
       </div>
     </div>
   );
--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function RightBar() {
   return (
-    <div className="flex flex-col right-0 max-w-100 mr-10 border-l-2 border-gray-300 pl-2">
+    <div className="flex flex-col right-0 max-w-100 mr-10 border-l-2 border-gray-300 pl-2 sticky top-0 h-screen">
       More to explore
       <div className="flex flex-col text-sm mt-2">
         <Link href="">Lee Know's cats</Link>
