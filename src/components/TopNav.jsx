1a9c9cee9f35dd93c20ce5d4446cfbe508c8cce3
dakshs-07 <dakshpinjore@gmail.com>
2026-02-16 06:28:55 +0530

    added quiz redirect in the navabr

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -6,7 +6,7 @@ function TopNav() {
     <div className="flex justify-center items-center bg-black text-red-300 text-sm gap-x-3 py-2">
       <Link href="">Stray Kids</Link>
       <Link href="">Noisey</Link>
-      <Link href="">Quizzes</Link>
+      <Link href="/quiz">Quizzes</Link>
       <Link href="">Albums</Link>
       <Link href=""></Link>
       <Link href=""></Link>
