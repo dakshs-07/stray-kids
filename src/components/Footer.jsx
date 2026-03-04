a867ac4d09d327b0e0f330cf4127fb94261dda0f
dakshs-07 <dakshpinjore@gmail.com>
2026-03-04 13:19:17 +0530

    updated footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,59 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <div className="bg-black text-white">
-      <div className="relative flex justify-center mt-30">
-        <div className="flex justify-around items-center w-full h-40 gap-x-10 sm:h-20 md:h-30 lg:h-40 mx-60">
-          <div className="flex flex-col">
-            <Link href="">About</Link>
-            <Link href="">About</Link>
+    <div className="bg-black text-white px-20 py-12 text-sm">
+      <div className="flex justify-between">
+        <div className="flex flex-col space-y-2">
+          <Link href="" className="hover:underline">
+            About
+          </Link>
+          <Link href="" className="hover:underline">
+            Contact
+          </Link>
+          <Link href="" className="hover:underline">
+            Contribute
+          </Link>
+          <Link href="" className="hover:underline">
+            Contact
+          </Link>
+        </div>
+        <div className="flex space-x-16">
+          <div className="flex flex-col space-y-2">
+            <Link href="" className="hover:underline">
+              Blog
+            </Link>
+            <Link href="" className="hover:underline">
+              About
+            </Link>
           </div>
-          <div className=" flex flex-col">
-            <Link href="">About</Link>
-            <Link href="">About</Link>
-            <Link href="">About</Link>
+          <div className="flex flex-col space-y-2">
+            <Link href="" className="hover:underline">
+              Lee Know Quiz
+            </Link>
+            <Link href="" className="hover:underline">
+              Stray Kids Quiz
+            </Link>
+            <Link href="" className="hover:underline">
+              Seungmin Quiz
+            </Link>
+            <Link href="" className="hover:underline">
+              Felix Quiz
+            </Link>
+            <Link href="" className="hover:underline">
+              Bangchan Quiz
+            </Link>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <Link href="" className="hover:underline">
+              Privacy Policy
+            </Link>
+            <Link href="" className="hover:underline">
+              Terms
+            </Link>
           </div>
         </div>
-        {/* <Image
-          src="/main/logo.png"
-          alt="stray-kids-footer image"
-          height={200}
-          width={200}
-        /> */}
       </div>
-      <p className="font-extralight text-center tracking-wider py-2">
+      <p className="font-extralight text-center traking-wider pt-10">
         CC 2026 Stray Kids Fandom
       </p>
     </div>
