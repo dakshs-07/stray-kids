16b7c8ef4420f35c20aafc2aa8b5e52cb5b2f32c
dakshs-07 <dakshpinjore@gmail.com>
2026-03-10 12:10:18 +0530

    added icons and imporved logic for dark mode

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,9 +1,16 @@
 "use client";
+import { Moon, Sun } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 function TopNav() {
+  const [dark, setDark] = useState(false);
+  function toggleDark() {
+    setDark(!dark);
+    document.documentElement.classList.toggle("dark");
+  }
   return (
-    <div className=" relative flex justify-center items-center bg-black text-red-300 text-sm py-2">
+    <div className="relative flex justify-center items-center bg-black text-red-300 text-sm py-2">
       <div className="flex gap-x-3">
         <Link href="">Stray Kids</Link>
         <Link href="">Noisey</Link>
@@ -12,11 +19,8 @@ function TopNav() {
         <Link href=""></Link>
         <Link href=""></Link>
       </div>
-      <button
-        onClick={() => document.documentElement.classList.toggle("dark")}
-        className="absolute right-4 cursor-pointer"
-      >
-        Dark Mode
+      <button onClick={toggleDark} className="absolute right-4 hover:cursor-pointer hover:brightness-90">
+        {dark ? <Sun size={20} /> : <Moon size={20} />}
       </button>
     </div>
   );
