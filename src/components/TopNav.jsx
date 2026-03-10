ef898eee341d1b1687d919269bfb591f2f35ef78
dakshs-07 <dakshpinjore@gmail.com>
2026-03-10 12:17:52 +0530

    added hover effects to topbar links

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -12,14 +12,25 @@ function TopNav() {
   return (
     <div className="relative flex justify-center items-center bg-black text-red-300 text-sm py-2">
       <div className="flex gap-x-3">
-        <Link href="">Stray Kids</Link>
-        <Link href="">Noisey</Link>
-        <Link href="/quiz">Quizzes</Link>
-        <Link href="">Albums</Link>
+        <Link href="" className="hover:brightness-125">
+          Stray Kids
+        </Link>
+        <Link href="" className="hover:brightness-125">
+          Noisey
+        </Link>
+        <Link href="/quiz" className="hover:brightness-125">
+          Quizzes
+        </Link>
+        <Link href="" className="hover:brightness-125">
+          Albums
+        </Link>
         <Link href=""></Link>
         <Link href=""></Link>
       </div>
-      <button onClick={toggleDark} className="absolute right-4 hover:cursor-pointer hover:brightness-90">
+      <button
+        onClick={toggleDark}
+        className="absolute right-4 hover:cursor-pointer hover:brightness-90"
+      >
         {dark ? <Sun size={20} /> : <Moon size={20} />}
       </button>
     </div>
